2706cf13d7ef94e0fc8754aa96d546cc83ec9b95
Sung Paik <sung.paik@codeimmersives.com>
2020-04-16 16:01:40 -0400

    first commit

---
+++ src/containers/Spotify/Spotify.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Spotify extends Component {
+    state = {
+        serverData: {}
+    };
+
+    render() {
+        return (
+            <h1>Spotify</h1>
+        )
+    }
+}
+
+export default Spotify;
